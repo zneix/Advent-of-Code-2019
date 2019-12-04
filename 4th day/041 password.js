7e68f840bf9f291756c9359e9feeb005161f71b9
Paweł <44851575+zneix@users.noreply.github.com>
2019-12-04 15:37:41 +0100

    2:57h/3:26h, 8142nd/7314th

---
+++ 4th day/041 password.js
@@ -0,0 +1,22 @@
+let min = 158126;
+let max = 624574;
+let sum = 0;
+
+for (let i=min;i<max;i++){
+    if (dC(i) && noDec(i)) sum++;
+}
+function dC(num){
+    let n = num.toString().split('');
+    for (i=0;i<5;i++){
+        if (n[i] == n[i+1]) return true;
+    }
+    return false;
+}
+function noDec(num){
+    let n = num.toString().split('').reverse();
+    for (j=0;j<5;j++){
+        if (n[j] < n[j+1]) return false;
+    }
+    return true;
+}
+console.log(sum);
