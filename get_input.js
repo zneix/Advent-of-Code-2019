00537c2f19ddb4c1ff3ea85a891b20c3b86df651
Paweł <44851575+zneix@users.noreply.github.com>
2019-12-08 18:48:48 +0100

    added script for automatic input fetch

---
+++ get_input.js
@@ -0,0 +1,17 @@
+let {exec} = require('child_process');
+let fs = require('fs');
+getInput(process.argv[2], process.argv[3]);
+function getInput(day, year){
+    if (!day) return console.log('[ERR] Day is undefined!');
+    if (!year) year = 2019;
+    let cookie = process.env.aoccookie;
+    let cmd = `curl https://adventofcode.com/${year}/day/${day}/input --cookie session=${cookie}`;
+    return exec(cmd, (err, stdout, info) => {
+        if (err) return console.log(`[ERR] ${err}`);
+        let errmsg = "Please don't repeatedly request this endpoint before it unlocks! The calendar countdown is synchronized with the server time; the link will be enabled on the calendar the instant this puzzle becomes available.\n";
+        if (stdout == errmsg) return console.log('[ERR] N/A yet!');
+        if (stdout == '404 Not Found\n') return console.log('[ERR] Error 404!');
+        let filename = 'input';
+        return fs.writeFile(`${process.cwd()}\\${filename}`, stdout.toString(), () => console.log(`[OK!] ${day}/${year}\n[OK!] ${process.cwd()}\\${filename} (${parseInt(stdout.length-1)})`));
+    });
+}
